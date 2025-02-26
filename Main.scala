d887320d436004ef81f94ad304a8a9aa714c57cc
Your Name <bullback16@gmail.com>
2025-02-26 14:16:14 +0900

    Add Main.scala with example Scala code

---
+++ Main.scala
@@ -0,0 +1,8 @@
+object Main { // 객체로 정의
+  def main(args: Array[String]): Unit = {
+    val name: String = "Alice"  // 이름 변수
+    var age: Int = 25           // 나이 변수
+
+    println(s"이름: $name, 나이: $age") // 출력
+  }
+}
